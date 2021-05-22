9021d151995adaecf6cd91fad12f9aca36f04422
Aleksandra Lukic <aleksandralukic@Aleksandras-Air.home>
2021-05-22 12:51:59 +0200

    Changed to test my kubernetes

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Fib Calculator</h1>
+          <h1 className="App-title">FIB CALCUTATOR KUBERNETES</h1>
           <a className="App-link"
             href="https://reactjs.org"
             target="_blank"
